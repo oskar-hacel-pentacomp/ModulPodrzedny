4173753b43366315ddf4cc84156a8acb79cddfaa
Oskar Hącel <oskar.hacel@pentacomp.pl>
2021-03-04 19:44:46 +0100

    Biblioteka Wspólna

---
+++ BibliotekaWspolna/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BibliotekaWspolna
+{
+    public class Test
+    {
+        public void TestBibliotekiWspolnej()
+        {
+            Console.WriteLine("Biblioteka wspólna działa prawidłowo");
+        }
+    }
+}
